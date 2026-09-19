559b9ff403d9a9bbcbdc61af2cf0140aecd00e28
samucarapo <samuelreidelas22brabo@gmail.com>
2026-09-19 17:36:50 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,10 +1,8 @@
-local HttpService = game:GetService("HttpService")
 local TextChatService = game:GetService("TextChatService")
 
-local url = "https://raw.githubusercontent.com/LDNOOBW/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/refs/heads/master/pt"
-local message = game:HttpGet(url)
+local message = "Eu sou gay"
 
 while true do
     TextChatService.TextChannels.RBXGeneral:SendAsync(message)
-    task.wait(0.1)
+    task.wait(1)
 end

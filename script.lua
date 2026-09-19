38c71fd91d748a320344996bd93da3c2258f4563
samucarapo <samuelreidelas22brabo@gmail.com>
2026-09-19 17:37:57 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,11 @@
+local Players = game:GetService("Players")
 local TextChatService = game:GetService("TextChatService")
+
+local player = Players.LocalPlayer
+
+if player.DisplayName:lower() ~= "paulo" then
+    return
+end
 
 local message = "Eu sou gay"
 
